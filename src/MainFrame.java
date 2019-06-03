63fc99dcbbc9f0feeb1f3e6b8603196d374d05f7
prembhusal <remp.bhusal@gmail.com>
2019-06-03 10:59:47 -0400

    button clickable

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,10 +16,16 @@ public class MainFrame extends JFrame {
 		textarea = new JTextArea();
 		btn = new JButton("click here");
 		
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textarea.append("first program\n");
+			}
+			
+		});
+		
+
 		add(textarea, BorderLayout.CENTER);
 		add(btn,BorderLayout.SOUTH);
-
-
 		setSize(600,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
