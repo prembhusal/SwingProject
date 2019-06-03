59fa8b91439b080665a4c40fca89806dfbb7095f
prembhusal <remp.bhusal@gmail.com>
2019-06-03 10:49:51 -0400

    button added

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,25 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
-public MainFrame() {
-	super("test app");
-	setSize(600,500);
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setVisible(true);
-}
+	private JTextArea textarea;
+	private JButton btn;
+	public MainFrame() {
+		super("test app");
+
+		setLayout(new BorderLayout());
+		textarea = new JTextArea();
+		btn = new JButton("click here");
+		
+		add(textarea, BorderLayout.CENTER);
+		add(btn,BorderLayout.SOUTH);
+
+
+		setSize(600,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
