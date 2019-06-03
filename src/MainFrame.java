e9bbb0576a643c3d594addd20df42c0a2fdea6c9
prembhusal <remp.bhusal@gmail.com>
2019-06-03 10:31:47 -0400

    initial swing project

---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+public MainFrame() {
+	super("test app");
+	setSize(600,500);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setVisible(true);
+}
+}
---
+++ src/TestApp.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new MainFrame();
+				
+			}
+			
+		});
+		
+	}
+
+}
