89331212d79245117927324762694463ca1280c9
prembhusal <remp.bhusal@gmail.com>
2019-06-03 12:03:24 -0400

    add text to panel

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,24 +7,25 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
-	private JTextArea textarea;
+	private TextPanel textPanel;
 	private JButton btn;
 	public MainFrame() {
 		super("test app");
 
 		setLayout(new BorderLayout());
-		textarea = new JTextArea();
+		textPanel = new TextPanel();
 		btn = new JButton("click here");
 		
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textarea.append("first program\n");
+				//textarea.append("first program\n");
+				textPanel.appendText("this is it\n");
 			}
 			
 		});
 		
 
-		add(textarea, BorderLayout.CENTER);
+		add(textPanel, BorderLayout.CENTER);
 		add(btn,BorderLayout.SOUTH);
 		setSize(600,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
